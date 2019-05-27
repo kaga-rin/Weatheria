0f165d63ee7ff476bc826b1427d828dac1f215c9
Rian Arfiandi <rian@Elisa.local>
2019-05-27 08:15:49 +0700

    Weatheria Release

--- app/Http/Controllers/Api.php
+++ app/Http/Controllers/Api.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
 class Api extends Controller
 {
+    private $apikey = "22a005eb0c37f87cdc0cf4bc3e949956";
+    private $weatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+    private $forecastUrl = "http://api.openweathermap.org/data/2.5/forecast";
+    private $lang = 'id';
+    private $units = 'metric';
+
     public function __construct()
     {
         $this->client = new Client();
@@ -16,12 +23,68 @@ class Api extends Controller
         return $this->suggestCity($name);
     }
 
-    public function suggestCity($name = null){
+    public function suggestCity($name = null)
+    {
         if (!empty($name)) {
-            $response = $this->client->request("GET", "https://kagarin.id/api/private/weatheria/?key=ump&kota={$name}");
+            $response = $this->client->request('GET', 'https://script.google.com/macros/s/AKfycbz_fJdc6fbg4vp-0vBS973_CMZbs_xjY9ZSzBgnpV8QujDiLCM/exec',
+                [
+                    'query' => [
+                        'key' => 'ump',
+                        'q' => $name,
+                    ],
+                ]);
             $body = $response->getBody();
             return response($body)->header('Content-Type', 'application/json');
-        }else{
+        } else {
+            return response()->json(
+                [
+                    "code" => "Taboo 04",
+                    "message" => "cant provide",
+                ]
+            );
+        }
+    }
+
+    public function current(Request $request)
+    {
+        if (!empty($request->input('lat')) && !empty($request->input('lon'))) {
+            $response = $this->client->request('GET', $this->weatherUrl,
+                [
+                    'query' => [
+                        'APPID' => $this->apikey,
+                        'lang' => $this->lang,
+                        'units' => $this->units,
+                        'lat' => $request->input('lat'),
+                        'lon' => $request->input('lon'),
+                    ],
+                ]);
+            $body = $response->getBody();
+            return response($body)->header('Content-Type', 'application/json');
+        } else {
+            return response()->json(
+                [
+                    "code" => "Taboo 04",
+                    "message" => "cant provide",
+                ]
+            );
+        }
+    }
+
+    public function predict(Request $request){
+        if (!empty($request->input('lat')) && !empty($request->input('lon'))) {
+            $response = $this->client->request('GET', $this->forecastUrl,
+                [
+                    'query' => [
+                        'APPID' => $this->apikey,
+                        'lang' => $this->lang,
+                        'units' => $this->units,
+                        'lat' => $request->input('lat'),
+                        'lon' => $request->input('lon'),
+                    ],
+                ]);
+            $body = $response->getBody();
+            return response($body)->header('Content-Type', 'application/json');
+        } else {
             return response()->json(
                 [
                     "code" => "Taboo 04",
@@ -31,4 +94,3 @@ class Api extends Controller
         }
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,14 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('current', 'Api@current');
+    $router->get('predict', 'Api@predict');
+});
 $router->get('/api[/{name}]', 'Api@index');
+
+
 $router->get('/{route:.*}/', function ()  {
     return view('index');
 });
